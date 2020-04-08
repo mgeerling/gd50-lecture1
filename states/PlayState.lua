23ef26c0a6e769029280c8ad3b6cbc428cfa2f67
Matt Geerling <mgeerling@mgeerling-macbookpro3.roam.corp.google.com>
2020-04-07 19:33:24 -0700

    updated playstate

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -22,6 +22,7 @@ function PlayState:init()
     self.pipePairs = {}
     self.timer = 0
     self.score = 0
+    self.isPaused = false
 
     -- initialize our last recorded Y value for a gap placement to base other gaps off of
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
@@ -33,77 +34,88 @@ function PlayState:update(dt)
 
     -- go back to play if enter is pressed
     if love.keyboard.wasPressed('p') then
-        gStateMachine:change('pause')
+        if self.isPaused == false then 
+            --gStateMachine:change('pause')
+            --TODO pause logic
+            self.isPaused = true
+            scrolling = false
+            --TODO render text 
+            --TODO bird DY to 0 ? 
+        else
+            self.isPaused = false
+        end
     end
-
-    -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
-        -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
-        -- no higher than 10 pixels below the top edge of the screen,
-        -- and no lower than a gap length (90 pixels) from the bottom
-        local y = math.max(-PIPE_HEIGHT + 10, 
-            math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-        self.lastY = y
-
-        -- add a new pipe pair at the end of the screen at our new Y
-        table.insert(self.pipePairs, PipePair(y))
-
-        -- reset timer
-        self.timer = 0
-    end
-
-    -- for every pair of pipes..
-    for k, pair in pairs(self.pipePairs) do
-        -- score a point if the pipe has gone past the bird to the left all the way
-        -- be sure to ignore it if it's already been scored
-        if not pair.scored then
-            if pair.x + PIPE_WIDTH < self.bird.x then
-                self.score = self.score + 1
-                pair.scored = true
-                sounds['score']:play()
+    if self.isPaused == false then 
+        -- spawn a new pipe pair every second and a half
+        if self.timer > 2 then
+            -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
+            -- no higher than 10 pixels below the top edge of the screen,
+            -- and no lower than a gap length (90 pixels) from the bottom
+            local y = math.max(-PIPE_HEIGHT + 10, 
+                math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            self.lastY = y
+    
+            -- add a new pipe pair at the end of the screen at our new Y
+            table.insert(self.pipePairs, PipePair(y))
+    
+            -- reset timer
+            self.timer = 0
+        end
+    
+        -- for every pair of pipes..
+        for k, pair in pairs(self.pipePairs) do
+            -- score a point if the pipe has gone past the bird to the left all the way
+            -- be sure to ignore it if it's already been scored
+            if not pair.scored then
+                if pair.x + PIPE_WIDTH < self.bird.x then
+                    self.score = self.score + 1
+                    pair.scored = true
+                    sounds['score']:play()
+                end
+            end
+    
+            -- update position of pair
+            pair:update(dt)
+        end
+    
+        -- we need this second loop, rather than deleting in the previous loop, because
+        -- modifying the table in-place without explicit keys will result in skipping the
+        -- next pipe, since all implicit keys (numerical indices) are automatically shifted
+        -- down after a table removal
+        for k, pair in pairs(self.pipePairs) do
+            if pair.remove then
+                table.remove(self.pipePairs, k)
             end
         end
-
-        -- update position of pair
-        pair:update(dt)
-    end
-
-    -- we need this second loop, rather than deleting in the previous loop, because
-    -- modifying the table in-place without explicit keys will result in skipping the
-    -- next pipe, since all implicit keys (numerical indices) are automatically shifted
-    -- down after a table removal
-    for k, pair in pairs(self.pipePairs) do
-        if pair.remove then
-            table.remove(self.pipePairs, k)
-        end
-    end
-
-    -- simple collision between bird and all pipes in pairs
-    for k, pair in pairs(self.pipePairs) do
-        for l, pipe in pairs(pair.pipes) do
-            if self.bird:collides(pipe) then
-                sounds['explosion']:play()
-                sounds['hurt']:play()
-
-                gStateMachine:change('score', {
-                    score = self.score
-                })
+    
+        -- simple collision between bird and all pipes in pairs
+        for k, pair in pairs(self.pipePairs) do
+            for l, pipe in pairs(pair.pipes) do
+                if self.bird:collides(pipe) then
+                    sounds['explosion']:play()
+                    sounds['hurt']:play()
+    
+                    gStateMachine:change('score', {
+                        score = self.score
+                    })
+                end
             end
         end
+    
+        -- update bird based on gravity and input
+        self.bird:update(dt)
+    
+        -- reset if we get to the ground
+        if self.bird.y > VIRTUAL_HEIGHT - 15 then
+            sounds['explosion']:play()
+            sounds['hurt']:play()
+    
+            gStateMachine:change('score', {
+                score = self.score
+            })
+        end
     end
-
-    -- update bird based on gravity and input
-    self.bird:update(dt)
-
-    -- reset if we get to the ground
-    if self.bird.y > VIRTUAL_HEIGHT - 15 then
-        sounds['explosion']:play()
-        sounds['hurt']:play()
-
-        gStateMachine:change('score', {
-            score = self.score
-        })
-    end
+    --Q? do you need an else where you remove the DY and other stuff
 end
 
 function PlayState:render()
@@ -121,12 +133,13 @@ end
 --[[
     Called when this state is transitioned to from another state.
 ]]
-function PlayState:enter(params)
+--TODO can readd params if needed 
+function PlayState:enter()
     -- if we're coming from death, restart scrolling
     -- if params.previousState ~= 'pause' then 
     --     self.score = params.score
     -- end 
-    scrolling = true
+    --scrolling = true
 end
 
 --[[
@@ -134,5 +147,5 @@ end
 ]]
 function PlayState:exit()
     -- stop scrolling for the death/score screen
-    scrolling = false
+    --scrolling = false
 end
